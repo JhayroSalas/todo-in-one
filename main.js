fa9eda405bf2003042c260860020f95cec8b1f5b
JhayroSalas <148156726+JhayroSalas@users.noreply.github.com>
2023-10-16 16:48:32 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,21 @@
+document.addEventListener('DOMContentLoaded', function() {
+    // Agrega el código que ya tienes aquí
+
+    // ...
+
+    let listElements = document.querySelectorAll('.list_button--click');
+
+    listElements.forEach(listElement => {
+        listElement.addEventListener('click', () => {
+            listElement.classList.toggle('arrow');
+
+            let height = 0;
+            let menu = listElement.nextElementSibling;
+            if(menu.clientHeight === 0) {
+                height = menu.scrollHeight;
+            }
+
+            menu.style.height = height + 'px';
+        });
+    });
+});
